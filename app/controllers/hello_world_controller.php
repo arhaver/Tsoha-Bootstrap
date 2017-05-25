3d99e70ea5d3656cf705c3a1f942544cff32c775
Arto Haverinen <arto.haverinen@helsinki.fi>
2017-05-25 15:57:58 +0300

    Näkymiä jatkuu

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,28 +4,28 @@ class HelloWorldController extends BaseController {
 
     public static function index() {
         // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-        echo 'Tämä on etusivu!';
+        View::make('suunnitelmat/index.html');
     }
 
     public static function sandbox() {
         // Testaa koodiasi täällä
         View::make('helloworld.html');
     }
-    
-    public static function exam_list(){
-    View::make('suunnitelmat/exam_list.html');
-  }
 
-  public static function exam_show(){
-    View::make('suunnitelmat/exam_show.html');
-  }
-  
-  public static function exam_edit(){
-    View::make('suunnitelmat/exam_edit.html');
-  }
+    public static function exam_list() {
+        View::make('suunnitelmat/exam_list.html');
+    }
 
-  public static function login(){
-    View::make('suunnitelmat/login.html');
-  }
+    public static function exam_show() {
+        View::make('suunnitelmat/exam_show.html');
+    }
+
+    public static function exam_edit() {
+        View::make('suunnitelmat/exam_edit.html');
+    }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
 
 }
