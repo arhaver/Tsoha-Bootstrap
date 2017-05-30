a0b1673886e03bfad50d22120430f9f926182157
Arto Haverinen <arto.haverinen@helsinki.fi>
2017-05-30 19:15:53 +0300

    Viikko2 valmis

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -4,9 +4,9 @@ INSERT INTO Person (name, password) VALUES ('Arha', 'Kissa123');
 INSERT INTO Person (name, password) VALUES ('Kapy', 'Olli123');
 
 -- Exam-taulun testidata
-INSERT INTO Exam (topic, testdate, testtime, room, tester, publicity) VALUES ('TestiTesti', '05/06/2017', '16:00', 'Excatum B123', 'Joku Proffa', true);
-INSERT INTO Exam (topic, testdate, testtime, room, tester) VALUES ('TestiTesti2', '06/05/2017', '9:00', 'Excatum B111', 'Liina Lehtori');
+-- INSERT INTO Exam (topic, testdate, testtime, room, tester, publicity) VALUES ('TestiTesti', '05/06/2017', '16:00', 'Excatum B123', 'Joku Proffa', true);
+-- INSERT INTO Exam (topic, testdate, testtime, room, tester) VALUES ('TestiTesti2', '06/05/2017', '9:00', 'Excatum B111', 'Liina Lehtori');
 
 -- Material-taulun testidata
-INSERT INTO Material (topic, writer, kind, publication, published) VALUES ('Testauksen ABC', 'Toimi Toimittaja', 'Artikkeli', 'Testaus raamaattu', 2014);
-INSERT INTO Material (topic) VALUES ('Tentti Kirja 1');
+-- INSERT INTO Material (topic, writer, kind, publication, published) VALUES ('Testauksen ABC', 'Toimi Toimittaja', 'Artikkeli', 'Testaus raamaattu', 2014);
+-- INSERT INTO Material (topic) VALUES ('Tentti Kirja 1');
