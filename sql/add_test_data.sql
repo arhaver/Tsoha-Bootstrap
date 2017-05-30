ce6f8991229473c466f3e18cc32a10874e28ef53
Arto Haverinen <arto.haverinen@helsinki.fi>
2017-05-30 19:02:16 +0300

    Viikko2 valmis

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -4,17 +4,13 @@ INSERT INTO Person (name, password) VALUES ('Arha', 'Kissa123');
 INSERT INTO Person (name, password) VALUES ('Kapy', 'Olli123');
 
 -- Exam-taulun testidata
-INSERT INTO Exam (owner, topic, testdate, testtime, room, tester, publicity) VALUES (1, 'TestiTesti', '05/06/2017', '16:00', 'Excatum B123', 'Joku Proffa', true);
-INSERT INTO Exam (owner, topic, testdate, testtime, room, tester) VALUES (2, 'TestiTesti2', '06/05/2017', '9:00', 'Excatum B111', 'Liina Lehtori');
+INSERT INTO Exam (topic, testdate, testtime, room, tester, publicity) VALUES ('TestiTesti', '05/06/2017', '16:00', 'Excatum B123', 'Joku Proffa', true);
+INSERT INTO Exam (topic, testdate, testtime, room, tester) VALUES ('TestiTesti2', '06/05/2017', '9:00', 'Excatum B111', 'Liina Lehtori');
 
 -- Material-taulun testidata
 INSERT INTO Material (topic, writer, kind, publication, published) VALUES ('Testauksen ABC', 'Toimi Toimittaja', 'Artikkeli', 'Testaus raamaattu', 2014);
 INSERT INTO Material (topic) VALUES ('Tentti Kirja 1');
 
 -- ExamMaterial-taulun testidata
-INSERT INTO ExamMaterial (exam, material, limitation, pages) VALUES (1, 2, 'Sivut 35-97', 63);
-INSERT INTO ExamMaterial (exam, material, pages) VALUES (2, 1, 15);
-
--- Testee-taulun testidata
-INSERT INTO Testee (exam, person) VALUES (1, 2);
-INSERT INTO Testee (exam, person) VALUES (2, 1);
+INSERT INTO ExamMaterial (limitation, pages) VALUES ('Sivut 35-97', 63);
+INSERT INTO ExamMaterial (pages) VALUES (15);
